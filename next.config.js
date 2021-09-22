4f07b51591b6fdadd0dd45657cff60e56d792168
rodriguez-luis <pb.rndm@gmail.com>
2021-09-22 03:09:21 +0000

    feat: update folder structure

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
-/** @type {import('next').NextConfig} */
+const path = require("path");
 module.exports = {
-  reactStrictMode: true,
+    reactStrictMode: true,
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'styles')],
+        prependData: @import "variables.scss";@import "mixins.scss";
+    },
+    env: {
+        FRONT_END_URL: process.env.FRONT_END_URL,
+        BACK_END_URL: process.env.BACK_END_URL,
+        EDIT_MODE: process.env.EDIT_MODE
+    }
 }
