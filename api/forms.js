ddb7c06410be3360b83e1ef698294cc824f752bd
rodriguez-luis <pb.rndm@gmail.com>
2021-09-22 16:17:17 +0100

    feat: add form  service

---
+++ api/forms.js
@@ -0,0 +1,20 @@
+export const register = async (form) => {
+    const request
+        = await fetch(
+            `${process.env.BACK_END_URL}/forms`,
+        {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            method: "POST",
+            body: JSON.stringify(form)
+        });
+    const response = await request.json();
+    return response.data;
+};
+
+export const read = async () => {
+    const request  = await fetch('http://localhost:8080/forms');
+    const response = await request.json();
+    return response;
+}
