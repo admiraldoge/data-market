04ba232ef547ffea12e82d4c3e0198acb1c5c1d9
rodriguez-luis <pb.rndm@gmail.com>
2021-11-10 20:19:19 +0100

    fix: update report api

--- api/reports.js
+++ api/reports.js
@@ -2,3 +2,9 @@ export const downloadCollectorSubmission = (collectorId) => {
   const url = `${process.env.BACK_END_URL}/reports/collectors/${collectorId}/submissions/csv`
   window.open(url);
 };
+
+export const readUserReport = async (userId) => {
+  const request = await fetch(`${process.env.BACK_END_URL}/reports/users/${userId}`);
+  const response = await request.json();
+  return response;
+};
